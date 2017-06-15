5b88dfa32869cc2da82d53572157eb00c6cc0ba3
xia <xiashim@163.com>
2017-06-15 16:51:16 +0800

    Net Core Test

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator=new FibonacciGenerator();
+            foreach (var digit in generator.Generate(15))
+            {
+                Console.WriteLine(digit);
+            }
+            // if(args.Length>0)
+            // {
+            //     Console.WriteLine($"Hello {args[0]}!");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Hello!");
+            // }
+
+            // Console.WriteLine("Fibonacci Number 1-15:");
+            
+            // for(int i=0;i<15;i++)
+            // {
+            //     Console.WriteLine($"{i+1}:{FibonacciNumber(i)}");
+            // }
+        }
+
+        static int FibonacciNumber(int n)
+        {
+            int a=0;
+            int b=1;
+            int tmp;
+            for(int i=0;i<n;i++)
+            {
+                tmp=a;
+                a=b;
+                b+=tmp;
+            }
+            return a;
+        }
+    }
+}
